8a927fd5cf2a3d5e0ecfcf33fe39b39960624021
Julie Sultanova <iuliiasult@gmail.com>
2024-01-15 13:01:11 +0900

    users sign-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,31 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
-import BookCard from "./BookCard";
-//import Auth from "./Auth";
+import Auth from "./Auth";
+import AddBooks from "./AddBooks";
 //import Account from "./Account";
 
-function App() {
-  //const [session, setSession] = useState(null);
-  //useEffect(() => {
-  //  supabase.auth.getSession().then(({ data: { session } }) => {
-  //    setSession(session);
-  //  });
-
-  //  supabase.auth.onAuthStateChange((_event, session) => {
-  //    setSession(session);
-  //  });
-  //}, []);
-  //return (
-  //  <div className="container" style={{ padding: "50px 0 100px 0" }}>
-  //    {!session ? (
-  //      <Auth />
-  //    ) : (
-  //      <Account key={session.user.id} session={session} />
-  //    )}
-  //  </div>
-  //);
-
-  //別のtableでやってみる
-  const [fetchError, setFetchError] = useState(null);
-  const [books, setBooks] = useState(null);
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState("");
-  const [formError, setFormError] = useState(null);
-
+export default function App() {
+  const [session, setSession] = useState(null);
   useEffect(() => {
-    const fetchBooks = async () => {
-      const { data, error } = await supabase.from("books").select();
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-      if (error) {
-        setFetchError("Something went wrong");
-        setBooks(null);
-        console.log(error);
-      }
-
-      if (data) {
-        setBooks(data);
-        setFetchError(null);
-      }
-    };
-
-    fetchBooks();
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
   }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!title || !rating) {
-      setFormError("Fill in all the fields");
-      return;
-    }
-
-    const { data, error } = await supabase
-      .from("books")
-      .insert([{ title, rating }])
-      .select();
-
-    if (error) {
-      console.log(error);
-      setFormError("Fill in all the fields");
-    }
-
-    if (data) {
-      console.log(data);
-      setFormError(null);
-    }
-  };
-
   return (
-    <div className="page">
-      {fetchError && <p>{fetchError}</p>}
-      {books && (
-        <div className="books">
-          <div className="books-grid">
-            {books.map((book) => (
-              <BookCard key={book.id} book={book} />
-            ))}
-          </div>
-        </div>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        //<Account key={session.user.id} session={session} />
+        <AddBooks session={session}/>
       )}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label htmlFor="rating">Rating:</label>
-        <input
-          type="number"
-          id="rating"
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
-        />
-        <button>本を追加する</button>
-        {formError && <p className="error">{formError}</p>}
-      </form>
     </div>
   );
 }
-export default App;
+
